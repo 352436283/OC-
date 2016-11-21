7be09c514bf49126d5b84ae08a5c02a096cbbf18
zhangsan <zhangsan@163.com>
2016-11-21 20:41:40 +0800

    初始化项目

---
+++ BanTang_OCyuyang/BanTang_OCyuyang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BanTang_OCyuyang
+//
+//  Created by qianfeng on 16/11/21.
+//  Copyright © 2016年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
